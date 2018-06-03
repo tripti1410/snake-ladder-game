e7008fe04f7db571e2307579ab0c893228a29251
Trapti Rahangdale <imtrapti1410@gmail.com>
2018-06-04 01:35:52 +0530

    added logic if number near around 100

--- main.js
+++ main.js
@@ -1,59 +1,83 @@
-function rollDice () {
-  return Math.floor(Math.random()*6+1);
+function rollDice() {
+  return Math.floor(Math.random() * 6 + 1);
 }
 
-let playerOneScore = 0, playerTwoScore = 0
+let playerOneScore = 0,
+  playerTwoScore = 0;
 
-function PlayerOne () {
+function PlayerOne() {
   const number = rollDice();
-//to show the player number
+  //to show the player number
   document.querySelector(".js-dice-result").innerHTML = number + "  player 1";
   //clear the old box
   if (playerOneScore > 0) {
     let getBoardItem = `.board-item-${playerOneScore}`;
-  document.querySelector(getBoardItem).style.setProperty('--ticker-color', 'transparent')
+    document
+      .querySelector(getBoardItem)
+      .style.setProperty("--ticker-color", "transparent");
   }
-  
+
   //set current box
-  playerOneScore = playerOneScore + number;
-  let getNewBoardItem = `.board-item-${playerOneScore}`
-  document.querySelector(getNewBoardItem).style.setProperty('--ticker-color', 'red');
-  if(playerOneScore > 94 && playerOneScore <= 100 ) {
-    
-    if (playerOneScore === 99 && number === 1) {
-      playerOneScore = playerOneScore + number
+
+  if (playerOneScore > 94 && playerOneScore <= 100) {
+    if (
+      (playerOneScore === 99 && number === 1) ||
+      (playerOneScore === 98 && number === 2) ||
+      (playerOneScore === 97 && number === 3) ||
+      (playerOneScore === 96 && number === 4) ||
+      (playerOneScore === 95 && number === 5)
+    ) {
+      console.log("inside 99");
+      playerOneScore = playerOneScore + number;
+      alert("player 1 winner");
     } else {
-      playerOneScore
+      playerOneScore;
     }
-    
-    if (playerOneScore === 98 && number === (1 || 2)) {
-      playerOneScore = playerOneScore + number
+
+    if (playerOneScore === 98 && number === 1) {
+      playerOneScore = playerOneScore + number;
     } else {
-      playerOneScore
+      playerOneScore;
     }
-    
-    if (playerOneScore === 97 && number === (1 || 2 || 3)) {
-      playerOneScore = playerOneScore + number
+
+    if (playerOneScore === 97 && (number === 1 || number === 2)) {
+      playerOneScore = playerOneScore + number;
     } else {
-      playerOneScore
+      playerOneScore;
     }
-    
-    if (playerOneScore === 96 && number === (1 || 2 || 3 || 4)) {
-      playerOneScore = playerOneScore + number
+
+    if (
+      playerOneScore === 96 &&
+      (number === 1 || number === 2 || number === 3)
+    ) {
+      console.log("inside 96");
+      playerOneScore = playerOneScore + number;
     } else {
-      playerOneScore
+      playerOneScore;
     }
-    
-    if (playerOneScore === 95 && number === (1 || 2 || 3 || 4 || 5)) {
-      playerOneScore = playerOneScore + number
+
+    if (
+      playerOneScore === 95 &&
+      (number === 1 || number === 2 || number === 3 || number === 4)
+    ) {
+      console.log("inside 95");
+      playerOneScore = playerOneScore + number;
     } else {
-      playerOneScore
+      playerOneScore;
     }
+    let getNewBoardItem = `.board-item-${playerOneScore}`;
+    document
+      .querySelector(getNewBoardItem)
+      .style.setProperty("--ticker-color", "red");
+  } else {
+    playerOneScore = playerOneScore + number;
+    let getNewBoardItem = `.board-item-${playerOneScore}`;
+    document
+      .querySelector(getNewBoardItem)
+      .style.setProperty("--ticker-color", "red");
   }
 }
 
-function PlayerTwo () {
+function PlayerTwo() {
   document.querySelector(".js-dice-result").innerHTML = rollDice();
 }
-
-
