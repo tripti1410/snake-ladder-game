4f3487994aa5feb9e01f2124002ca895b5a40c14
Trapti Rahangdale <imtrapti1410@gmail.com>
2018-06-04 02:27:21 +0530

    added button classes and enable and disable conditions when user switch

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ function rollDice() {
   return Math.floor(Math.random() * 6 + 1);
 }
 
-let playerOneScore = 0,
-  playerTwoScore = 0;
+let playerOneScore = 0, playerTwoScore = 0
 
 function PlayerOne() {
   const number = rollDice();
   //to show the player number
+  if (number === 6) {
+    document.querySelector(".js-button-2").classList.add("disabled");
+    document.querySelector(".js-button-1").classList.remove("disabled");
+  } else {
+    document.querySelector(".js-button-1").classList.add("disabled");
+    document.querySelector(".js-button-2").classList.remove("disabled");
+  }
+
   document.querySelector(".js-dice-result").innerHTML = number + "  player 1";
   //clear the old box
   if (playerOneScore > 0) {
@@ -79,5 +86,85 @@ function PlayerOne() {
 }
 
 function PlayerTwo() {
-  document.querySelector(".js-dice-result").innerHTML = rollDice();
+  const number = rollDice();
+
+  if (number === 6) {
+    document.querySelector(".js-button-1").classList.add("disabled");
+    document.querySelector(".js-button-2").classList.remove("disabled");
+  } else {
+    document.querySelector(".js-button-2").classList.add("disabled");
+    document.querySelector(".js-button-1").classList.remove("disabled");
+  }
+
+  //to show the player number
+  document.querySelector(".js-dice-result").innerHTML = number + "  player 2";
+  //clear the old box
+  if (playerTwoScore > 0) {
+    let getBoardItem = `.board-item-${playerTwoScore}`;
+    document
+      .querySelector(getBoardItem)
+      .style.setProperty("--ticker-color", "transparent");
+  }
+
+  //set current box
+
+  if (playerTwoScore > 94 && playerTwoScore <= 100) {
+    if (
+      (playerTwoScore === 99 && number === 1) ||
+      (playerTwoScore === 98 && number === 2) ||
+      (playerTwoScore === 97 && number === 3) ||
+      (playerTwoScore === 96 && number === 4) ||
+      (playerTwoScore === 95 && number === 5)
+    ) {
+      console.log("inside 99");
+      playerTwoScore = playerTwoScore + number;
+      alert("player 1 winner");
+    } else {
+      playerTwoScore;
+    }
+
+    if (playerTwoScore === 98 && number === 1) {
+      playerTwoScore = playerTwoScore + number;
+    } else {
+      playerTwoScore;
+    }
+
+    if (playerTwoScore === 97 && (number === 1 || number === 2)) {
+      playerTwoScore = playerTwoScore + number;
+    } else {
+      playerTwoScore;
+    }
+
+    if (
+      playerTwoScore === 96 &&
+      (number === 1 || number === 2 || number === 3)
+    ) {
+      console.log("inside 96");
+      playerTwoScore = playerTwoScore + number;
+    } else {
+      playerTwoScore;
+    }
+
+    if (
+      playerTwoScore === 95 &&
+      (number === 1 || number === 2 || number === 3 || number === 4)
+    ) {
+      console.log("inside 95");
+      playerTwoScore = playerTwoScore + number;
+    } else {
+      playerTwoScore;
+    }
+    let getNewBoardItem = `.board-item-${playerTwoScore}`;
+    document
+      .querySelector(getNewBoardItem)
+      .style.setProperty("--ticker-color", "green");
+  } else {
+    playerTwoScore = playerTwoScore + number;
+    let getNewBoardItem = `.board-item-${playerTwoScore}`;
+    document
+      .querySelector(getNewBoardItem)
+      .style.setProperty("--ticker-color", "green");
+  }
 }
+
+
